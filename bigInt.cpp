bb1fdfd0e4ec558a1b67ab61f2f1b534b1fd22e9
Zhehao Lu <zhehaolu@usc.edu>
2018-04-28 23:22:16 -0700

    Doing comparators

--- bigInt.cpp
+++ bigInt.cpp
@@ -9,8 +9,8 @@ BigInt::BigInt(const BigInt &i)
   : P_Int(i), neg_(i.neg_) { }
 
 BigInt::BigInt(int i) 
-  : P_Int(abs(i)) {
-  neg_ = i > 0? false: true;
+  : P_Int(static_cast<unsigned>(abs(i))) {
+  neg_ = i < 0? true: false;
 }
 
 BigInt& BigInt::operator=(const BigInt& rhs) {
@@ -27,4 +27,101 @@ std::ostream& operator<<(std::ostream& os, const BigInt &rhs) {
 		os << (int) rhs.at(len - j - 1);
 	return os;
 }
+
+BigInt& BigInt::operator++() {
+  if (neg_)
+    P_Int::operator--();
+  else
+    P_Int::operator++();
+  if (P_Int::operator==(0))
+    neg_ = false;
+  return *this;
+}
+
+BigInt BigInt::operator++(int o) {
+  BigInt copy(*this);
+  this->operator++();
+  return copy;
+}
+
+BigInt& BigInt::operator--() {
+  // if non-positive, then add to the abs value and set to negative
+  if (neg_ || P_Int::operator==(0)) {
+    P_Int::operator++();
+    neg_ = false;
+  }
+  else {
+    P_Int::operator--();
+  }
+  return *this;
+}
+
+BigInt BigInt::operator--(int o) {
+  BigInt copy(*this);
+  this->operator--();
+  return copy;
+}
+
+/**
+ * Comparisons
+ */
+bool BigInt::operator==(const BigInt &rhs) const {
+  if (neg_ != rhs.neg_) return false;
+  if (!P_Int::operator==(rhs)) return false;
+  return true;
+}
+
+bool BigInt::operator!=(const BigInt &rhs) const {
+  return !operator==(rhs);
+}
+
+bool BigInt::operator<(const BigInt &rhs) const {
+  // Different signs
+  if (neg_ && !rhs.neg_) return true;
+  else if (!neg_ && rhs.neg_) return false;
+  if (!neg_)
+    return P_Int::operator<(rhs);
+  else
+    return P_Int::operator>(rhs);
+}
+
+bool BigInt::operator>(const BigInt &rhs) const {
+  return !(operator<(rhs) || operator==(rhs));
+}
+
+bool BigInt::operator<=(const BigInt &rhs) const {
+  return (operator<(rhs) || operator==(rhs));
+}
+
+bool BigInt::operator>=(const BigInt &rhs) const {
+  return !operator<(rhs);
+}
+
+// below are friend functions
+bool operator>(int lhs, const BigInt &rhs) {
+  return rhs.operator<(lhs);
+}
+
+bool operator<(int lhs, const BigInt &rhs) {
+  return rhs.operator>(lhs);
+}
+
+bool operator>=(int lhs, const BigInt &rhs) {
+  return rhs.operator<=(lhs);
+}
+
+bool operator<=(int lhs, const BigInt &rhs) {
+  return rhs.operator>=(lhs);
+}
+
+bool operator==(int lhs, const BigInt &rhs) {
+  return rhs.operator==(lhs);
+}
+
+bool operator!=(int lhs, const BigInt &rhs) {
+  return rhs.operator!=(lhs);
+}
+/**
+ * Comparisons end
+ */
 
