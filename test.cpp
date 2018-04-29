82b1fb48c23e3926de6c52d93aecb8f7cc3bdd85
Zhehao Lu <zhehaolu@usc.edu>
2018-04-28 23:23:04 -0700

    Ignoring VSCode config files

--- test.cpp
+++ test.cpp
@@ -1,11 +1,10 @@
 #include "bigInt.h"
+#include "p_int.h"
 #include <iostream>
 using namespace std;
 
 int main() {
-	BigInt i(-123);
-	BigInt j(330);
-	BigInt k(i);
-	cout << i << endl;
+	BigInt i(-59), j(-58);
+	cout << boolalpha << (i < j) << endl;
 	return 0;
 }
